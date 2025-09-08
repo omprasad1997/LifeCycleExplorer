6a19ee9cf3e7ba8b29307d37520a194ecb05abba
omprasadrathod <20rathodomprasad@gmail.com>
2025-09-08 19:31:19 +0530

    Fragment Transaction + Activity Switch

--- app/src/main/java/com/example/lifecycleexplorer/MainActivity.kt
+++ app/src/main/java/com/example/lifecycleexplorer/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.lifecycleexplorer.LifecycleLogger.TAG
 import com.example.lifecycleexplorer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
-        //LifecycleLogger.log(TAG, "MainActivity onCreate")
+        LifecycleLogger.log(TAG, "MainActivity onCreate")
 
         replaceFragment(FragmentA())
 
@@ -31,6 +32,37 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragmentContainer, fragment)
             .commit()
     }
+
+    override fun onStart() {
+        super.onStart()
+        LifecycleLogger.log(TAG, "MainActivity onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LifecycleLogger.log(TAG, "MainActivity onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        LifecycleLogger.log(TAG, "MainActivity onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        LifecycleLogger.log(TAG, "MainActivity onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        LifecycleLogger.log(TAG, "MainActivity onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LifecycleLogger.log(TAG, "MainActivity onDestroy")
+    }
+
 
 
 }
